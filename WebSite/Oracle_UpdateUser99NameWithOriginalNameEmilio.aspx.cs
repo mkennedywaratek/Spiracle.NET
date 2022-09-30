ac50ac410fc380804fe594d26f32e62e8e26e269
mkennedy <mkennedy@waratek.com>
2022-09-30 14:50:56 +0100

    QA-1402. Changes for stored XSS test case

---
+++ WebSite/Oracle_UpdateUser99NameWithOriginalNameEmilio.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+
+namespace WebSite
+{
+    public partial class Oracle_UpdateUser99NameWithOriginalNameEmilio : OraclePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var sqlCmd = "update users set users.name = 'Emilio' where users.id = 99";
+            sqlQuery.Text = sqlCmd; 
+            
+            var connStr = ConfigurationManager.ConnectionStrings["OracleLocalConnection"].ToString();
+            using (OracleConnection connnection = new OracleConnection(connStr))
+            {
+                connnection.Open();
+                var sqlCommand = new OracleCommand(sqlCmd, connnection);
+                sqlCommand.ExecuteReader();
+            }
+        }
+    }
+}
